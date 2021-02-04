b47f5ca90f168ca8aaad033519835acd9b7aa93a
alexhkurz <alexhkurz@gmail.com>
2021-02-04 11:24:58 -0800

    two planes

--- two_planes.m
+++ two_planes.m
@@ -1,9 +1,15 @@
-X = linspace(-20,20,100);
-Y = linspace(-20,20,100);
+# THE GRID
+X = Y = linspace(-10,10,100);
 [ x, y ] = meshgrid(X,Y);
-z1 = 1 - x - y;
-z2 = 4/3 - 2/3 * x + 1/3 *y;
-figure;
+
+# THE EQUATIONS
+z1 = x - y;
+z2 = - x + y;
+
+# PLOTTING
+figure(1,"position",get(0,"screensize"))
 hold on;
-mesh(x,y,z1);
-mesh(x,y,z2);
+mesh(x,y,z1,'FaceColor', [0.5 1.0 0.5]);
+mesh(x,y,z2,'FaceColor', [1.0 0.5 0.0]);
+set(gca, "linewidth", 2, "fontsize", 12);
+xlabel ("x-axis","fontsize", 20); ylabel ("y-axis","fontsize", 20); zlabel("z-axis","fontsize", 20);
